7d1121636cbf3aa547b4d6df1ee3f40fe7024509
bangpii <baihaqiearrafi6@gmail.com>
2025-10-05 00:25:51 +0700

    Update User.jsx

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -7,10 +7,10 @@ const User = () => {
   return (
     <div>
       <img className='absolute top-0 right-0 opacity-60-z-10'
-      src="../public/gradient.png" alt="Background" />
+      src="../../public/gradient.png" alt="Background" />
 
       {/* Aura */}
-      <div className='h-0 w-[40rem] absolute top-[20%] right-[-5%] shadow-[0_0_900px_20px_#e99b63] -rotate-[30deg -z-10]'></div>
+      <div className='h-0 w-[40rem] absolute top-[20%] right-[-5%] shadow-[0_0_900px_20px_#e99b63] -rotate-[30deg] -z-10'></div>
       
 
       <Header/>
